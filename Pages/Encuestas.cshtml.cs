400d69fa4c58dd026f0985bb4e8b7ce7bacc20a9
Roger Lozano <lozanofierroruu@gmail.com>
2025-08-05 18:50:18 +0200

    Se ha creado toda la lógica de la encuesta

--- Pages/Encuestas.cshtml.cs
+++ Pages/Encuestas.cshtml.cs
@@ -1,8 +1,24 @@
+using Encuesta.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Encuesta.Pages;
 
 public class EncuestaModel : PageModel
 {
+    [BindProperty]
+    public Encuesta.Models.Encuesta encuesta { get; set; } = new Encuesta.Models.Encuesta();
 
+    private readonly ServicioEncuestas _servicioEncuestas;
+
+    public EncuestaModel(ServicioEncuestas servicioEncuestas)
+    {
+        _servicioEncuestas = servicioEncuestas;
+    }
+
+    public IActionResult OnPost()
+    {
+        _servicioEncuestas.Agregar(encuesta);
+        return RedirectToPage("Gracias");
+    }
 }
