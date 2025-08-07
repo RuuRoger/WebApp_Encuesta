e7a4b75e6b61bd1eecac129ed57d7e751eef6fea
Roger Lozano <lozanofierroruu@gmail.com>
2025-08-07 19:08:36 +0200

    Se ha creado el HTML de Gracias y resultados, con la lógica de la página 'Resultados'.

--- Pages/Resultados.cshtml.cs
+++ Pages/Resultados.cshtml.cs
@@ -1,8 +1,23 @@
+using Encuesta.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Encuesta.Pages;
 
 public class ResultadosModel : PageModel
 {
+    private readonly ServicioEncuestas _servicioEncuestas;
+
+    public ResultadosModel(ServicioEncuestas servicioEncuestas)
+    {
+        _servicioEncuestas = servicioEncuestas;
+    }
+
+
+    public List<Encuesta.Models.Encuesta> Encuestas { get; set; } = new List<Encuesta.Models.Encuesta>();
+
+    public void OnGet()
+    {
+        Encuestas = _servicioEncuestas.Encuestas;
+    }
 
 }
